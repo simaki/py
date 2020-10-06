2465ea7f39b9b50ea896b827ce4940c0e46170e6
Shota Imaki <shota.imaki@icloud.com>
2020-10-06 11:30:13 +0900

    autoformat

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ python3 -m pytest --doctest-modules project-name
 python3 -m pytest --doctest-modules tests
 
 python3 -m flake8 project-name
-python3 -m black --check project-name
-python3 -m isort --check --force-single-line-imports project-name
+python3 -m black --check project-name || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black project-name
+python3 -m isort --check --force-single-line-imports project-name || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports project-name
