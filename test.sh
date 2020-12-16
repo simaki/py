c99e0b493041d5e3ab6d9fc3b0ef881b4360d6a4
Shota Ray Imaki <shota.imaki@icloud.com>
2020-12-16 16:46:19 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python3 -m pytest --doctest-modules project-name
+python3 -m pytest --doctest-modules PROJECT_NAME
 python3 -m pytest --doctest-modules tests
 
-python3 -m flake8 project-name
-python3 -m black --check project-name || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black project-name
-python3 -m isort --check --force-single-line-imports project-name || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports project-name
+python3 -m flake8 PROJECT_NAME
+python3 -m black --check PROJECT_NAME || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black PROJECT_NAME
+python3 -m isort --check --force-single-line-imports PROJECT_NAME || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports PROJECT_NAME
